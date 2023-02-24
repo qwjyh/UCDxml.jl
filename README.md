de55483a282d2c6facf04c2c8a4660418ed45fb8
qwjyh <urataw421@gmail.com>
2023-02-24 15:39:36 +0900

    improve doc

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://qwjyh.github.io/UCDxml.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://qwjyh.github.io/UCDxml.jl/dev/)
 [![Build Status](https://github.com/qwjyh/UCDxml.jl/actions/workflows/CI.yml/badge.svg?branch=master)](https://github.com/qwjyh/UCDxml.jl/actions/workflows/CI.yml?query=branch%3Amaster)
+
+UCD data from xml.
--- src/parser.jl
+++ src/parser.jl
@@ -2,7 +2,13 @@ import EzXML
 
 ##############################################################
 # code point set
+"""
+    abstract type CodePointsSet end
 
+There are two types of code point representation.
+`SingleCodePoint` and `RangeCodePoint` with `first` and `last` codepoint.
+Each codepoint value is `UInt32` (as it is expressed as hexadecimal number with four to six digits, ie. minimum is `0x0000` and maximum is `0xFFFFFF`(UAX#44 #4.2.2))
+"""
 abstract type CodePointsSet end
 struct SingleCodePoint <: CodePointsSet
     cp::UInt32
@@ -12,7 +18,7 @@ struct RangeCodePoint <: CodePointsSet
     last::UInt32
 end
 
-function get_codepoint(node::EzXML.Node)
+function get_codepoint(node::EzXML.Node)::CodePointsSet
     cps = try
         cp = parse(UInt32, node["cp"], base = 16)
         SingleCodePoint(cp)
@@ -35,6 +41,16 @@ end
 ###############################################################
 # code point type
 
+"""
+    @enum CodePointType begin
+        reserved
+        noncharacter
+        surrogate
+        char
+    end
+
+CodePoint type enum.
+"""
 @enum CodePointType begin
     reserved
     noncharacter
@@ -69,6 +85,18 @@ function get_name(node::EzXML.Node)::String
 end
 
 # name alias
+# TODO: replace type with enum
+"""
+    struct NameAlias <: Property
+        alias::String
+        type::String
+    end
+
+name alias
+# Fields
+- `alias::String`: name
+- `type::String`: type of alias("abbreviation", "alternative", "control", "correction" or "figment")
+"""
 struct NameAlias <: Property
     alias::String
     type::String
@@ -97,12 +125,50 @@ function get_block(node::EzXML.Node)::String
     end
 end
 
+"""
+module for GeneralCategories.
+To separate namespace for General Category enum.
+"""
 module GeneralCategories
 
 using EzXML
 
 export GeneralCategory, get_generalcategory
 
+"""
+    @enum GeneralCategory begin
+        Lu # Letter, uppercase
+        Ll # Letter, lowercase
+        Lt # Letter, titlecase
+        Lm # Letter, modifier
+        Lo # Letter, other
+        Mn # Mark, nonspacing
+        Mc # Mark, spacing combining
+        Me # Mark, enclosing
+        Nd # Number, decimal digit
+        Nl # Number, letter
+        No # Number, other
+        Pc # Punctuation, connector
+        Pd # Punctuation, dash
+        Ps # Punctuation, open
+        Pe # Punctuation, close
+        Pi # Punctuation, initial quote
+        Pf # Punctuation, final quote
+        Po # Punctuation, other
+        Sm # Symbol, math
+        Sc # Symbol, currency
+        Sk # Symbol, modifier
+        So # Symbol, other
+        Zs # Separator, space
+        Zl # Separator, line
+        Zp # Separator, paragraph
+        Cc # Other, control
+        Cf # Other, format
+        Cs # Other, surrogate
+        Co # Other, private use
+        Cn # Other, not assigned (including noncharacters)
+    end
+"""
 @enum GeneralCategory begin
     Lu # Letter, uppercase
     Ll # Letter, lowercase
@@ -454,17 +520,19 @@ using .GeneralCategories
 
 
 """
+    struct UCDRepertoireNode
+
 UAX#42
 # Fields
-- `type`: `char`, `reserved`, `noncharacter`, `surrogate`
-- `cp`: single code point or range with first, last
-- `na`: standard name
+- `type::CodePointType`: `char`, `reserved`, `noncharacter`, `surrogate`
+- `cp::CodePointsSet`: single code point or range with first, last
+- `na::String`: standard name
 - `na1`: 1.0 name
-- `name_alias`: zero or more
+- `name_alias::Vector{NameAlias}`: zero or more
     - `alias`: String
     - `type`: "abbreviation", "alternate", ... etc
-- `blk`: block
-- `gc`: general category (enum)
+- `blk::String`: block
+- `gc::GeneralCategory`: general category (enum)
 - `ccc`: canonical combining class in decimal
 - `bidi`: bidirectional properties (see `BidirectionalProperties` doc for more details)
 - `decomp`: see DecompositionProperties
